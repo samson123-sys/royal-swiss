cc78427370599e91e21d0e595415cec44d17da5a
Sam Ayebanate <samuelnate20@gmail.com>
2024-07-17 14:55:48 -0700

    implement initial file and folder structure

---
+++ app/(auths)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const signUp = () => {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default signUp
---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ 
+  subsets: ["latin"], 
+  variable: '--font-inter' });
+
+const ibm_Plex_Serif = IBM_Plex_Serif({
+   subsets: ["latin"], 
+   weight: ['400', '700'], 
+   variable: '--font-ibm-plex-serif'})
+
+export const metadata: Metadata = {
+  title: "Royal Swiss",
+  description: "Royal Swiss is a modern banking platform for the wealthy",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibm_Plex_Serif.variable}`}>{children}</body>
+    </html>
+  );
+}
